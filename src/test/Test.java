578e9d36acc4155e761aa273d66c22c6eac20d42
Rubing Duan <rubing.duan@gmail.com>
2012-07-06 00:07:53 +0800

    this is a test

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,11 @@
 package test;
 
 public class Test {
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		double[][] test = new double[3][3];
 		for (int i = 0; i < 3; i++) {
@@ -8,6 +13,7 @@ public class Test {
 				System.out.println(test[i][j]);
 			}
 		}
+		System.out.print("helo");
 	}
 
 }
